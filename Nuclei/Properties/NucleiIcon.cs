3c301142910d9e8b240a3642bbd94c31da90c186
Madalin Gheorghe <madalin.gheorghe89@gmail.com>
2026-07-06 14:50:49 +0300

    Fix v4 Grasshopper category abbreviation

--- Nuclei/Properties/NucleiIcon.cs
+++ Nuclei/Properties/NucleiIcon.cs
@@ -11,7 +11,7 @@ namespace Nuclei3.Properties
             public override Grasshopper.Kernel.GH_LoadingInstruction PriorityLoad()
             {
                 Grasshopper.Instances.ComponentServer.AddCategoryIcon("Nuclei4", Nuclei3.Properties.Resources.Nuclei2);
-                Grasshopper.Instances.ComponentServer.AddCategoryShortName("Nuclei4", "Nuclei4");
+                Grasshopper.Instances.ComponentServer.AddCategoryShortName("Nuclei4", "N4");
                 Grasshopper.Instances.ComponentServer.AddCategorySymbolName("Nuclei4", 'N');
 
                 return Grasshopper.Kernel.GH_LoadingInstruction.Proceed;
